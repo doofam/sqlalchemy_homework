ebaeb77e6e12cc3897fe2963bb656a62843670d5
Sinah Kang <sinahkang@Sinahs-MacBook-Pro.local>
2020-02-15 21:07:03 -0600

    homework revised

--- flask_app.py
+++ flask_app.py
@@ -20,16 +20,7 @@ session = Session(engine)
 
 app = Flask(__name__)
 
-class DictMixIn:
-    def to_dict(self):
-        return {
-            column.name: getattr(self, column.name)
-            if not isinstance(getattr(self, column.name), datetime.datetime)
-            else getattr(self, column.name).isoformat()
-            for column in self.__table__.columns
-        }
-
-class Measurement(Base, DictMixIn):
+class Measurement(Base):
     __tablename__ = "measurement"
     id = Column(Integer, primary_key=True)
     station = Column(String)
@@ -37,13 +28,14 @@ class Measurement(Base, DictMixIn):
     prcp = Column(Float)
     tobs = Column(Float)
 
-class Station(Base, DictMixIn):
+class Station(Base):
     __tablename__ = "station"
     id = Column(Integer, primary_key=True)
     station = Column(String)
     latitude = Column(Float)
     longitude = Column(Float)
     elevation = Column(Float)
+
 
 @app.route("/")
 def home():
@@ -58,36 +50,36 @@ def home():
         f"<br/>"
         f"NOTE: Dates should be formatted YYYY-MM-DD")
 
+
 @app.route("/api/v1.0/precipitation")
 def get_prcp_data():
     prcp_data = session.query(Measurement.date, Measurement.prcp).order_by(Measurement.date).all()
     return jsonify(prcp_data)
+
 
 @app.route("/api/v1.0/temperature")
 def get_temp_data():
     temp_data = session.query(Measurement.date, Measurement.tobs).order_by(Measurement.date).all()
     return jsonify(temp_data)  
 
-# temps = session.query(Measurement.date,
-#     label("temp_min", func.min(Measurement.tobs),
-#     label("temp_avg", func.avg(Measurement.tobs), 
-#     label("temp_max", func.max(Measurement.tobs))
-#     .filter(Measurement.date >= start_date).all()
 
 temps = session.query(Measurement.date, Measurement.tobs).all()
 df_temps = pd.DataFrame(temps)
 df_temps.loc[:, "date"] = pd.to_datetime(df_temps["date"], yearfirst=True)
+
 
 @app.route("/api/v1.0/<start>")
 def calc_temps_1(start):
     df_start = df_temps.loc[df_temps["date"] >= datetime.strptime(start, "%Y-%m-%d")]
     return f"Average: {df_start['tobs'].mean()}, Min: {df_start['tobs'].min()}, Max: {df_start['tobs'].max()}"
 
+
 @app.route("/api/v1.0/<start>/<end>")
 def calc_temps_2(start, end):
     df_start_end = df_temps.loc[(df_temps["date"] >= datetime.strptime(start, "%Y-%m-%d"))\
          & (df_temps["date"] <= datetime.strptime(end, "%Y-%m-%d"))]
     return f"Average: {df_start_end['tobs'].mean()}, Min: {df_start_end['tobs'].min()}, Max: {df_start_end['tobs'].max()}"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
